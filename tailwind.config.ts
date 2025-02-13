6d077b177f6f9aa4318d374898d9f078f271d083
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-02-13 15:22:52 +0700

    add: profile page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,20 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        greenBum: '#134739',
-        redBum: '#B2474D',
-        yellowBum: '#DDB343',
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        greenBum: "#134739",
+        redBum: "#B2474D",
+        "redBum-light": "#E96269",
+        yellowBum: "#DDB343",
       },
     },
   },
